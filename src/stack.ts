a6334c6de49b18d8185da7deafc64d43318b518b
usef <karimiusef679@gmail.com>
2024-10-19 17:45:53 +0330

    Implement Stack in TypeScript

---
+++ src/stack.ts
@@ -0,0 +1,40 @@
+
+class Stack<T> {
+    private items: T[];
+
+    constructor() {
+        this.items = [];
+    }
+
+    push(element: T): void {
+        this.items.push(element);
+    }
+
+    pop(): T | string {
+        if (this.isEmpty()) return "Underflow";
+        return this.items.pop() as T;
+    }
+
+    peek(): T | string {
+        if (this.isEmpty()) return "Underflow";
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty(): boolean {
+        return this.items.length === 0;
+    }
+
+    printStack(): string {
+        return this.items.join(" ");
+    }
+
+    size(): number {
+        return this.items.length;
+    }
+
+    reverse(): void {
+        this.items.reverse();
+    }
+}
+
+export default Stack;
